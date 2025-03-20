2f4c4b9478c6534c903102b72a9245dd4b4916d9
HerrBubimann <67808294+HerrBubimann@users.noreply.github.com>
2025-03-20 15:32:09 +0100

    bruch hinzu

--- Uebung_4.py
+++ Uebung_4.py
@@ -44,7 +44,36 @@ class Uebungen:
             # Gibt das Ergebnis nach jeder 'variable' aus
             print(ergebniss)
 
+class Katze:
+    # Konstruktor, initalisiert das Objekt
+    def __init__(self, name, farbe, alter=1, geschlecht = True):
+        self.name = name # Attribut
+        self.farbe = farbe
+        self.alter = alter
+        self.geschlecht = geschlecht
+
+    # weitere Methode
+    def gebe_aus(self):
+        print(f'Die Katze heisst {self.name} und ist {self.alter} Jahre alt und hat die Farbe {self.farbe}')
+
+    def __str__(self):
+        geschlecht_str = "männlich" if self.geschlecht else "weiblich"
+        return f"Katze: {self.name}, Alter: {self.alter}, Farbe: {self.farbe}, Geschlecht: {geschlecht_str}"
+
+    def miau(self):
+        print(f"{self.name} hat miauut")
+
+    @staticmethod
+    def main():
+        cat_1 = Katze(name='Mietze', alter=5, farbe='grau', geschlecht=True)
+        cat_2 = Katze(name='Mietze_2', alter=6, farbe='grau', geschlecht=False)
+        cat_1.gebe_aus()
+        cat_1.miau()
+        cat_2.miau()
+        cat_2.gebe_aus()
+
 
 if __name__ == '__main__':
-    uebungen_20_03 = Uebungen()
-    uebungen_20_03.main()
+    #uebungen_20_03 = Uebungen()
+    #uebungen_20_03.main()
+    Katze.main()
---
+++ Uebung_4_Bruch.py
@@ -0,0 +1,51 @@
+import math
+
+class Bruch:
+    def __init__(self, zaehler, nenner=1):
+        if nenner == 0:
+            raise ValueError("Nenner darf nicht null sein.")
+        self.zaehler = zaehler
+        self.nenner = nenner
+        self.kuerzen()
+
+    def kuerzen(self):
+        teiler = math.gcd(self.zaehler, self.nenner)
+        self.zaehler //= teiler
+        self.nenner //= teiler
+        if self.nenner < 0:
+            self.zaehler *= -1
+            self.nenner *= -1
+
+    def __str__(self):
+        return f"{self.zaehler}/{self.nenner}"
+
+    def __add__(self, bruch_addieren):
+        neuer_zaehler = self.zaehler * bruch_addieren.nenner + bruch_addieren.zaehler * self.nenner
+        neuer_nenner = self.nenner * bruch_addieren.nenner
+        return Bruch(neuer_zaehler, neuer_nenner)
+
+    def float_bruch(self):
+        return self.zaehler / self.nenner
+
+    @staticmethod
+    def main():
+        bruch1 = Bruch(3, 4)
+        bruch2 = Bruch(1, 2)
+        print(bruch1)
+        print(bruch2)
+
+        bruch3 = bruch1.__add__(bruch2)
+        print(bruch3)
+
+        bruch4 = Bruch(1, 3)
+        print(bruch4.float_bruch())
+
+        bruch1.__str__()
+        bruch2.__str__()
+        bruch3.__str__()
+        bruch4.__str__()
+
+
+
+if __name__ == '__main__':
+    Bruch.main()
