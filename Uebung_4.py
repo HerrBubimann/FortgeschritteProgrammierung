a56c46881883d2ec81bf949cd62f523fe9521bda
HerrBubimann <67808294+HerrBubimann@users.noreply.github.com>
2025-03-24 09:09:43 +0100

    anpassungen

--- Uebung_4.py
+++ Uebung_4.py
@@ -5,21 +5,18 @@ class Uebungen:
         """
         Static-Methode: Ersetzt bestimmte Zeichen im Text durch den Wert der 'variable'.
         """
-        # Dictionary mit Ersetzungen: Bestimmte Zeichen werden durch die 'variable' ersetzt
         reinfolge_zu_ersetzten = {
             "ie": variable, "au": variable, "eu": variable, "ei": variable,
             "a": variable, "e": variable, "o": variable, "u": variable,
             "ä": variable, "ö": variable, "ü": variable, "i": variable
         }
 
-        # Schleife über die Schlüssel im Dictionary, um die Ersetzungen vorzunehmen
         for key in reinfolge_zu_ersetzten:
             """ 
             Ersetzt alle Vorkommen von 'key' im Text durch den entsprechenden Wert (variable).
             """
             text = text.replace(key, reinfolge_zu_ersetzten[key])
 
-        # Gibt den modifizierten Text zurück
         return text
 
     @staticmethod
@@ -27,32 +24,24 @@ class Uebungen:
         """
         Hauptmethode: Testet die Ersetzungen für eine Liste von Wörtern mit verschiedenen 'variable' Werten.
         """
-        # Liste von Testwörtern, die bearbeitet werden sollen
         testworte = ["Drei", "Chinesen", "mit", "Kontrabass", "sassen", "auf", "der", "Strasse", "und", "erzählten",
                      "sich", "was"]
 
-        # Liste der zu ersetzenden Zeichen (Vokale und Umlaute)
         zu_ersetzten = ["a", "e", "o", "u"]
 
-        # Schleife über jede 'variable' in der Liste 'zu_ersetzten'
         for variable in zu_ersetzten:
             ergebniss = ""
-            # Schleife über jedes Wort in der Liste 'testworte'
             for wort in testworte:
-                #Ersetzt jedes Wort mit der Methode 'drei_chinesen' und fügt es zum Ergebnis hinzu.
                 ergebniss += " " + Uebungen.drei_chinesen(wort, variable)
-            # Gibt das Ergebnis nach jeder 'variable' aus
             print(ergebniss)
 
 class Katze:
-    # Konstruktor, initalisiert das Objekt
     def __init__(self, name, farbe, alter=1, geschlecht = True):
-        self.name = name # Attribut
+        self.name = name
         self.farbe = farbe
         self.alter = alter
         self.geschlecht = geschlecht
 
-    # weitere Methode
     def gebe_aus(self):
         print(f'Die Katze heisst {self.name} und ist {self.alter} Jahre alt und hat die Farbe {self.farbe}')
 
