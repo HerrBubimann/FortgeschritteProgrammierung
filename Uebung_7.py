2521ece6228dbecd4aa8f2ed5fe5c3f1ebb26471
HerrBubimann <67808294+HerrBubimann@users.noreply.github.com>
2025-04-02 11:45:08 +0200

    Queues

---
+++ Uebung_7.py
@@ -0,0 +1,71 @@
+from queue import Queue, LifoQueue
+
+class Uebung:
+
+    def FIFO(self):
+        # FIFO Queue
+        q1 = Queue()
+        q1.put(10)
+        print(q1)
+
+        q2 = Queue(maxsize=5)
+        q2.put(1)
+        q2.put(2)
+        q2.put(3)
+        q2.put(4)
+        q2.put(5)
+        print(q2.full())
+
+        item = q2.get()
+        print(item)
+
+        while not q2.empty():
+            print(f"The value is", q2.get())
+
+    @staticmethod
+    def LIFO():
+        stack = LifoQueue()
+        stack.put(1)
+        stack.put(2)
+        stack.put(3)
+
+        print(stack.get())
+        print(stack.get())
+        print(stack.get())
+        print(stack.empty())
+
+    @staticmethod
+    def q_4_erstellen():
+        q4 = Queue()
+        q4.put(11)
+        q4.put(5)
+        q4.put(4)
+        q4.put(21)
+        q4.put(3)
+        q4.put(10)
+        return q4
+
+    def revers_queue(self, qsrc, qdest, zaehler_laenge):
+        if not qsrc.empty():
+            zaehler_laenge.append(qsrc.qsize())
+            buffer = qsrc.get()
+            self.revers_queue(qsrc, qdest, zaehler_laenge)
+            qdest.put(buffer)
+            print(qdest.qsize())
+        return qdest
+
+    @staticmethod
+    def main():
+        zaehler_laenge = []
+        q4dest = Queue()
+        q4 = Uebung.q_4_erstellen()
+        uebung_instance = Uebung()
+        qReversed = uebung_instance.revers_queue(q4, q4dest, zaehler_laenge)
+        while not qReversed.empty():
+            print(qReversed.get())
+
+        print(len(zaehler_laenge), zaehler_laenge)
+
+if __name__ == "__main__":
+    uebung = Uebung()
+    uebung.main()
