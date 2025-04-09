be310756d9e4fa629e60989526dcdac81a50a924
HerrBubimann <67808294+HerrBubimann@users.noreply.github.com>
2025-04-09 15:49:34 +0200

    anpassungen

--- Uebung 8.py
+++ Uebung 8.py
@@ -1,3 +1,5 @@
+import time
+
 class HalfIterator:
     def __init__(self, array):
         self.array = array
@@ -15,3 +17,31 @@ class HalfIterator:
                 raise HalfIterator("Außerhalb des Wertebereichs")
         except Exception as e:
             raise HalfIterator(f"Fehler beim Iterieren: {str(e)}")
+
+class ReversIterator:
+    def __init__(self, array):
+        self.array = array
+        self.index = len(array) - 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        try:
+            if self.index >= 0:
+                print("print in if")
+                index = self.index
+                self.index -= 1
+                time.sleep(1)
+                return self.array[index]
+            else:
+                raise StopIteration
+        except Exception as e:
+            raise self.ReversIteratorError(f"Fehler beim Iterieren: {str(e)}")
+
+    class ReversIteratorError(Exception):
+        pass
+
+if __name__ == "__main__":
+    for test in ReversIterator([1,2,3]):
+        print(test)
